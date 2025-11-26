aca37f1cda8549d320b41f71da68b01a37a0faf3
SEMI-SAINT-GAMES <120226332+SEMI-SAINT-GAMES@users.noreply.github.com>
2025-11-26 15:48:50 +0200

    async pymongo

--- backend/core/db/mongo.py
+++ backend/core/db/mongo.py
@@ -1,16 +1,17 @@
 import os
-from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+from pymongo.asynchronous.mongo_client import AsyncMongoClient
+mongodb_client: AsyncMongoClient | None = None
+mongodb = None
 
-mongodb_client: AsyncIOMotorClient | None = None
-mongodb: AsyncIOMotorDatabase | None = None
+
 async def connect_to_mongo():
     print("Connecting to MongoDB...")
     print(os.getenv("MONGO_URL"))
     global mongodb_client, mongodb
     if mongodb_client is None:
         try:
-            mongodb_client = AsyncIOMotorClient(os.getenv("MONGO_URL"))
-            mongodb = mongodb_client[os.getenv("DATABASE_NAME")]
+            mongodb_client = AsyncMongoClient(os.getenv("MONGO_URL"))
+            mongodb = mongodb_client[os.getenv("MONGO_DB_NAME")]
             print("Connected to MongoDB!")
             print(mongodb)
             return mongodb
@@ -18,13 +19,15 @@ async def connect_to_mongo():
             print(f"Error connecting to MongoDB: {e}")
             raise e
 
+
 async def close_mongo():
     global mongodb_client
     if mongodb_client:
-        mongodb_client.close()
+        await mongodb_client.close()
         print("MongoDB connection closed")
 
-async def get_mongodb() -> AsyncIOMotorDatabase:
+
+async def get_mongodb():
     global mongodb
     if mongodb is None:
         await connect_to_mongo()
